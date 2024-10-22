335461f533464a65b71b7aebfe41c47e748e9c41
Bakhadir1988 <myblogfree1988@gmail.com>
2024-10-22 22:25:16 +0300

    add 13-query

---
+++ app.js
@@ -0,0 +1,9 @@
+const queryParams = (search, take) => {
+  const query = { search, take };
+  const queryString = Object.keys(query).map(value => `${value}=${query[value]}`).join('&');
+  return queryString;
+}
+
+const result = queryParams('Вася', 10);
+
+console.log(result);
