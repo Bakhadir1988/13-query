9997c86be724e52a97f50018989aa59f57c84e4d
Bakhadir1988 <myblogfree1988@gmail.com>
2024-10-28 23:56:04 +0300

    ДЗ: 13-query

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const queryParams = (search, take) => {
   const queryString = Object.keys(query).map(value => `${value}=${query[value]}`).join('&');
   return queryString;
 }
-
 const result = queryParams('Вася', 10);
 
 console.log(result);
